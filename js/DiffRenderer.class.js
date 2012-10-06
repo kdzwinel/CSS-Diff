d726e8cba2f9484928ea83a87186120655937409
Konrad Dzwinel <kdzwinel@gmail.com>
2012-10-06 20:56:22 +0200

    fixes #2 Differences displayed in a table fixes #3 Similar attributes grouped, webkit attributes separated.

---
+++ js/DiffRenderer.class.js
@@ -0,0 +1,101 @@
+function DiffRenderer(container) {
+    var suffixes = ["background", "margin", "padding", "border", "outline", "font", "listStyle", "overflow"];
+    var container = container;
+
+    //Generates reasonable name for element from its tag name, classes and id
+    var elementName = function(element) {
+        var name = element.tag;
+
+        if(element.id !== null) {
+            name += '#' + element.id;
+        } else if(element.class !== null) {
+            name += '.' + element.class.replace(/ /g,'.');
+        }
+
+        return name;
+    }
+
+    //Checks if attribute has a suffix and returnes it
+    var getSuffix = function(attrName) {
+        for(var idx in suffixes) {
+            var suffix = suffixes[idx];
+
+            if( attrName.length > suffix.length && attrName.substr(0, suffix.length) == suffix ) {
+                return suffix;
+            }
+        }
+
+        return null;
+    }
+
+    var isSuffix = function(attrName) {
+        return ( suffixes.indexOf(attrName) !== -1 );
+    }
+
+    this.render = function(element0, element1, diff) {
+        var buffer = "";
+        var webkitBuffer = "";
+
+        buffer += '<div class="block">';
+        buffer += '<p class="description">Comparing <strong class="element0">' + elementName(element0) + '</strong> ';
+        buffer += 'with <strong class="element1">' + elementName(element1) + '</strong>.</p>';
+
+        buffer += '<table>';
+        for(var name in diff) {
+            var value = diff[name];
+
+            var suffix = getSuffix(name);
+            var suffixClass = "";
+            var suffixParentAttributes = "";
+            var row = "";
+
+            if(suffix !== null) {
+                suffixClass = " suffix " + suffix;
+            }
+
+            if(isSuffix(name)) {
+                suffixParentAttributes = "class='suffix-parent' data-suffix='" + name + "'";
+            }
+
+            row += "<tr class='upper" + suffixClass + "'>";
+                row += "<th rowspan='2' " + suffixParentAttributes + ">" + name + "</th>";
+                row += "<td class='element0'>" + value[0] + "</td>";
+            row += "</tr>";
+            row += "<tr class='lower" + suffixClass + "'>";
+                row += "<td class='element1'>" + value[1] + "</td>";
+            row += "</tr>";
+
+            if(name.substr(0, 6) === "webkit") {
+                webkitBuffer += row;
+            } else {
+                buffer += row;
+            }
+        }
+        buffer += '</table></div>';
+
+        if(webkitBuffer.length > 0) {
+            buffer += "<div class='sidebar-separator'>WebKit attributes</div>";
+            buffer += "<div class='block'><table>";
+            buffer += webkitBuffer;
+            buffer += "</table></div>";
+        }
+
+        container.innerHTML = buffer;
+
+        //react to "onclick" events on parent elements
+        var suffixParents = container.getElementsByClassName('suffix-parent');
+        for (var i = 0, il = suffixParents.length; i < il ; i++) {
+            var suffixParent = suffixParents[i];
+
+            suffixParent.onclick = function() {
+                this.classList.contains("open") ? this.classList.remove("open") : this.classList.add("open");
+
+                var children = container.getElementsByClassName(this.dataset.suffix);
+
+                for (var j = 0, jl = children.length; j < jl ; j++) {
+                    children[j].style.display = (children[j].style.display !== "table-row") ? "table-row" : "none";
+                }
+            }
+        }
+    }
+}
---
+++ js/sidebar.js
@@ -0,0 +1,56 @@
+/**
+ * Returns JSON with CSS properties that differ between two given HTML elements.
+ */
+var page_compareElements = function(a, b) {
+    if(a === undefined || b === undefined) {
+        return null;
+    }
+
+    var aComputed = getComputedStyle(a);
+    var bComputed = getComputedStyle(b);
+    var diff = {};
+
+    for(var aname in aComputed) {
+        var avalue = aComputed[aname];
+        var bvalue = bComputed[aname];
+
+        if( aname === 'length' || aname === 'cssText' || typeof avalue === "function" || avalue == bvalue) {
+            continue;
+        }
+
+        diff[aname] = [avalue, bvalue]
+    }
+    
+    return {
+        element0: {
+            'tag': a.tagName,
+            'id': (a.attributes.id) ? a.attributes.id.value : undefined,
+            'class': (a.attributes.class) ? a.attributes.class.value : undefined
+        },
+        element1: {
+            'tag': b.tagName,
+            'id': (b.attributes.id) ? b.attributes.id.value : undefined,
+            'class': (b.attributes.class) ? b.attributes.class.value : undefined
+        },
+        'diff': diff
+    };
+}
+
+window.onload = function() {
+
+    var body = document.getElementsByTagName('body')[0];
+    var diffRenderer = new DiffRenderer(body);
+
+    function updateElementProperties() {
+        chrome.devtools.inspectedWindow.eval("(" + page_compareElements.toString() + ")($0, $1)", function (result) {
+            if(result !== null) {
+                diffRenderer.render( result.element0, result.element1, result.diff );
+            } else {
+                body.innerHTML = "<div class='info'>Nothing to compare. Inspect two elements first.</div>";
+            }
+        });
+    }
+    updateElementProperties();
+
+    chrome.devtools.panels.elements.onSelectionChanged.addListener(updateElementProperties);
+}
